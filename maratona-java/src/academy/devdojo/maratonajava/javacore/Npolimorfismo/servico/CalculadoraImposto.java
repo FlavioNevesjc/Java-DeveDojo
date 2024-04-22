5128d2468b1e059d9ad2182ef105fc75f42ebd0c
FlavioNevesjc <flaviosn@gmail.com>
2024-04-22 14:19:18 -0300

    Incluindo mais projetos de estudos da maratona Java - Iniciando Aula 97

--- maratona-java/src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Npolimorfismo/servico/CalculadoraImposto.java
@@ -1,20 +1,22 @@
 package academy.devdojo.maratonajava.javacore.Npolimorfismo.servico;
 
-import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 
 public class CalculadoraImposto {
-    public static void calcularImpostoComputador (Computador computador){
-        System.out.println("Relatório de imposto do computador");
-        double imposto = computador.calcularImposto();
 
-        System.out.println("Computador "+computador.getNome() +"\nValor "+computador.getValor()+"\nImposto a ser pago: "+imposto);
-    }
+    public static void calcularImposto(Produto produto){
+        System.out.println("Relatório de imposto");
+        double imposto = produto.calcularImposto();
 
-    public static void calcularImpostoTomate (Tomate tomate){
-        System.out.println("Relatório de imposto do tomate");
-        double imposto = tomate.calcularImposto();
+        System.out.println("Produto: "+produto.getNome() +"\nValor "+produto.getValor()+"\nImposto a ser pago: "+imposto);
+        if (produto instanceof Tomate) {
+            // Outra maneira de realizar
+            // System.out.println(((Tomate) produto).getDataValidade());
+            //
+            Tomate tomate = (Tomate) produto;
+            System.out.println(tomate.getDataValidade());
 
-        System.out.println("Computador "+tomate.getNome() +"\nValor "+tomate.getValor()+"\nImposto a ser pago: "+imposto);
+        }
     }
 }
---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/RepositorioTeste.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.repositorio.Repositorio;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioArquivo;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioBancoDeDados;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.RepositorioMemoria;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class RepositorioTeste {
+    public static void main(String[] args) {
+
+        Repositorio repositorio = new RepositorioBancoDeDados();
+        // Repositorio repositorio = new RepositorioMemoria();
+        // Repositorio repositorio = new RepositorioArquivo();
+
+        repositorio.salvar();
+
+        List<String> list = new LinkedList<>();
+        list.add("Goku");
+        list.add("Vegeta");
+        list.add("Kuririn");
+        System.out.println(list);
+    }
+}
