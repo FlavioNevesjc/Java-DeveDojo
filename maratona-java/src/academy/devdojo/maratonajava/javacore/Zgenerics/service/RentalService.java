abcd6e3caffaf7f8ea32d36d5e6ec2329748ccc5
FlavioNevesjc <flaviosn@gmail.com>
2024-10-29 11:34:11 -0300

    Até a aula 191 - Classes Internas pt3

---
+++ maratona-java/src/academy/devdojo/maratonajava/javacore/Zgenerics/service/RentalService.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.service;
+
+
+
+import java.util.List;
+
+public class RentalService<T> {
+    private List<T> objetosDisponiveis;
+
+    public RentalService(List<T> objetosDisponiveis){
+        this.objetosDisponiveis = objetosDisponiveis;
+    }
+
+
+    public T buscarObjetoDisponivel(){
+        System.out.println("Buscando Objeto disponivel...");
+        T t = objetosDisponiveis.remove(0);
+        System.out.println("Alugando Objeto: "+ t);
+        System.out.println("Objetos disponiveis para alugar: ");
+        System.out.println(objetosDisponiveis);
+        return t;
+    }
+
+    public void retornarObjetoAlugado(T t){
+        System.out.println("Devolvendo objeto "+ t);
+        objetosDisponiveis.add(t);
+        System.out.println("Objeto disponivel para alugar");
+        System.out.println(objetosDisponiveis);
+    }
+}
